bc8b26f908a9f2861e762e5cc375c70e321da9dc
Thatonerookiedev <shatoriag01@gmail.com>
2025-03-30 19:57:12 -0500

    Start of player movement
    For now the player just shots off the platform

--- block slider tutorial/Assets/PlayerMovement.cs
+++ block slider tutorial/Assets/PlayerMovement.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //a test to disable gravity https://www.youtube.com/watch?v=9ZEu_I-ido4&list=PLPV2KyIb3jR5QFsefuO2RlAgWEz6EvVi6&index=3
+    //3:40
+
+    public Rigidbody rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //this is excecuted when you start the game
     void Start()
     {
-        
+        //add small force
+        //rb.AddForce(0,200,500);
     }
 
     // Update is called once per frame
-    void Update()
+    //This function was called update before
+    //You can call it fixed update if you are working with pyhsics
+        //unity likes it better that was
+    void FixedUpdate()
     {
-        
+        //the original was bellow
+            //rb.AddForce(0,0,200);
+            //this is bad because it updates every seccond and computers will vary in frame rate!
+            //instead of a fixed value that can make the cube slower or faster depending on the computer use the below instead
+        rb.AddForce(0,0,2000 * Time.deltaTime);
     }
 }
