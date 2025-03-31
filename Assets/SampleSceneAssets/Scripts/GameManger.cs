e8de86de934940baffd7ed4196b0d4d7b4f49478
Thatonerookiedev <shatoriag01@gmail.com>
2025-03-31 11:52:20 -0500

    restart done

---
+++ Assets/SampleSceneAssets/Scripts/GameManger.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//Use this is you need to change to a different scene/level or reload/restart a scene/level 
+
+public class GameManger : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    private float restartDelay = 2f;
+    public void EndGame(){
+        if(gameHasEnded == false){
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            //Invoke will help you create a small delay without worrying
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
--- Assets/SampleSceneAssets/Scripts/PlayerMovement.cs
+++ Assets/SampleSceneAssets/Scripts/PlayerMovement.cs
@@ -41,5 +41,9 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.AddForce(-sidewaysForce * Time.deltaTime,0,0 ,ForceMode.VelocityChange);
         }
+
+        if(rb.position.y < -1f){
+            FindAnyObjectByType<GameManger>().EndGame();
+        }
     }
 }
